43089fc932b4cfcabb75eb18fd40a96985e7747f
John Wei <johnzw89@gmail.com>
2013-10-29 14:38:49 -0400

    adding init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reservester::Application.config.secret_token = '98f2a1840d2b88e6fa567147ce1390d041ab08870363a83f40e4c2cb5bd0d550ee8dfc5f64cd83c9aa506f1d17e91f5a764cbbabed3d74b4a4afb7a8ea6dd56f'
